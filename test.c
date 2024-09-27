832cfc9f7cb3f776a84c97246bc7b37a1bf3655e
Dananjaya Bandara <110759795+danjayz@users.noreply.github.com>
2024-09-27 11:47:44 +0530

    Initial commit

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int sum(int n) {
+    int s=0;
+    if (n == 1) {
+        return n;
+    } else {
+        return s=n+sum(n-1);
+    }
+}
+
+int main() {
+    printf("%d\n", sum(50));
+}
